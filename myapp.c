d9be5e168274ca9b751efc70669102e517c9841e
florianloga <florian.motloga@gmail.com>
2024-12-03 21:49:44 +0200

    Adaugat functii de rotire si echilibrul

--- myapp.c
+++ myapp.c
@@ -23,6 +23,41 @@ int maxim(int a,int b){
     return b;
 }
 
+struct Nod *rotireStanga(struct Nod *x)
+{
+    struct Nod *y = x->dreapta;
+    struct Nod *SS = y->stanga;
+
+    y->stanga = x;
+    x->dreapta = SS;
+
+    x->inaltime = maxim(inaltime(x->stanga), inaltime(x->dreapta)) + 1;
+    y->inaltime = maxim(inaltime(y->stanga), inaltime(y->dreapta)) + 1;
+
+    return y;
+}
+
+struct Nod *rotireDreapta(struct Nod *y)
+{
+    struct Nod *x = y->stanga;
+    struct Nod *SD = x->dreapta;
+
+    x->dreapta = y;
+    y->stanga = SD;
+
+    x->inaltime = maxim(inaltime(x->stanga), inaltime(x->dreapta)) + 1;
+    y->inaltime = maxim(inaltime(y->stanga), inaltime(y->dreapta)) + 1;
+
+    return x;
+}
+
+int echilibru(struct Nod *nod)
+{
+    if (nod == NULL)
+        return 0;
+    return inaltime(nod->stanga) - inaltime(nod->dreapta);
+}
+
 struct Nod* inserare(struct Nod* nod, int cheie){
     if(nod==NULL)
     return 0;
@@ -43,4 +78,11 @@ void populare(struct Nod** nod,int nr,int min,int max){
     for(i=0;i<nr;i++){
         int val=rand() % (max-min +1) + min;
     }
+}
+
+int main(){
+
+
+
+    return 0;
 }
